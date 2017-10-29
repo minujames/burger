1e06c141a65f0331870bab44da80467a03152c5f
minujames <minuemiliajames@gmail.com>
2017-10-28 21:38:58 -0700

    adding orm all function

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var connection = require("./connection.js");
+
+var orm = {
+  all: function(tableName, callBack){
+    var queryStr = "SELECT * FROM " + tableName + ";";
+    connection.query(queryStr, function(error, result){
+      if(error){
+        throw error;
+      }
+      if(typeof callBack === "function"){
+        callBack(result);
+      }
+    });
+  };
+};
+
+module.exports = orm;
